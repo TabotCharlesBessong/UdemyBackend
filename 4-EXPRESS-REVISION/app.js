4b31a3725bbd4cde46d7ce34dc03cee13e2ce4dd
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-05-03 13:29:50 +0100

    adding css staically

--- 4-EXPRESS-REVISION/app.js
+++ 4-EXPRESS-REVISION/app.js
@@ -8,6 +8,7 @@ const port = process.env.PORT || 5000
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
+app.use(express.static(path.join(__dirname,'public')))
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/admin', adminRoutes);
