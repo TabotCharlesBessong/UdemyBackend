2b732a05ef1d841256a1112c56b63dfdd70cb8e1
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-05-06 13:50:59 +0100

    putting some finishing touches

--- 9-DYNAMIC-ROUTING/controllers/shop.js
+++ 9-DYNAMIC-ROUTING/controllers/shop.js
@@ -33,22 +33,24 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-  Cart.getCart(cart =>{
+  Cart.getCart(cart => {
     Product.fetchAll(products => {
-      const cartProducts = []
+      const cartProducts = [];
       for (product of products) {
-        const cartProductData = cart.products.find(prop => prod.id === product.id )
-        if (cartProductData){
-          cartProducts.push({productData:product,qty:productData.qty})
+        const cartProductData = cart.products.find(
+          prod => prod.id === product.id
+        );
+        if (cartProductData) {
+          cartProducts.push({ productData: product, qty: cartProductData.qty });
         }
       }
       res.render('shop/cart', {
-      path: '/cart',
-      pageTitle: 'Your Cart',
-      products: cartProducts
+        path: '/cart',
+        pageTitle: 'Your Cart',
+        products: cartProducts
       });
-    })  
-  })  
+    });
+  });
 };
 
 exports.postCart = (req, res, next) => {
@@ -57,6 +59,14 @@ exports.postCart = (req, res, next) => {
     Cart.addProduct(prodId, product.price);
   });
   res.redirect('/cart');
+};
+
+exports.postCartDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId, product => {
+    Cart.deleteProduct(prodId, product.price);
+    res.redirect('/cart');
+  });
 };
 
 exports.getOrders = (req, res, next) => {
--- 9-DYNAMIC-ROUTING/models/cart.js
+++ 9-DYNAMIC-ROUTING/models/cart.js
@@ -45,6 +45,9 @@ module.exports = class Cart {
       }
       const updatedCart = { ...JSON.parse(fileContent) };
       const product = updatedCart.products.find(prod => prod.id === id);
+      if (!product) {
+          return;
+      }
       const productQty = product.qty;
       updatedCart.products = updatedCart.products.filter(
         prod => prod.id !== id
@@ -57,14 +60,14 @@ module.exports = class Cart {
       });
     });
   }
-  static getProducts(cb){
-    fs.writeFile(p, JSON.stringify(updatedCart), err => {
-      const cart = JSON.parse(fileContent)
-      if(err){
-        cb(null)
-      }else{
-        cb(cart)
-         
+
+  static getCart(cb) {
+    fs.readFile(p, (err, fileContent) => {
+      const cart = JSON.parse(fileContent);
+      if (err) {
+        cb(null);
+      } else {
+        cb(cart);
       }
     });
   }
