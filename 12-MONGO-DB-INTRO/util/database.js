f6268f0fd84d88c07d2f8190521b4014df626d30
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-05-12 19:22:29 +0100

    made the mongodb connection and sarted clearing out some more

--- 12-MONGO-DB-INTRO/util/database.js
+++ 12-MONGO-DB-INTRO/util/database.js
@@ -1,8 +1,15 @@
-const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+const mongodb = require('mongodb')
 
-module.exports = sequelize;
+const MongoClient = mongodb.MongoClient
+
+const mongoConnect = (callback)=>{
+  MongoClient.connect("mongodb+srv://tabotcharlesbessong:nlXAQiqmL4D4ZApT@cluster0.azbbe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority").then( client => {
+  console.log('connected');
+  }  ).catch(err => {
+    console.log(err)
+  } )
+  }
+
+
+module.exports = mongoConnect
