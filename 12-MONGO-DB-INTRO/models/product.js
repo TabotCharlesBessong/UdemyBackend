91f179af21dd5dcaa0fb2e0373af8cc5a5abb123
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-05-13 07:58:40 +0100

    added the fetch functionalities

--- 12-MONGO-DB-INTRO/models/product.js
+++ 12-MONGO-DB-INTRO/models/product.js
@@ -1,4 +1,5 @@
 // const mongoConnect = require('../util/database').mongoConnect
+const mongodb = require('mongodb')
 const getDB = require('../util/database').getDB
 
 class Product {
@@ -14,6 +15,34 @@ class Product {
     return  db.collection('products').insertOne(this).then(result  =>{
       console.log(result)
     } ).catch(err => {
+      console.log(err)
+    } )
+  }
+
+  static fetchAll(){
+    const db = getDB()
+    return db.collection('products')
+    .find()
+    .toArray()
+    .then( products => {
+      console.log(products)
+      return products
+    } )
+    .catch( err => {
+      console.log(err)
+    } )
+  }
+
+  static findById(prodId){
+    const db = getDB()
+    return db.collection('products')
+    .find({_id: new mongodb.ObjectId(prodId)})
+    .next()
+    .then( product =>{
+      console.log(product)
+      return product
+    } )
+    .catch( err => {
       console.log(err)
     } )
   }
