b750c1ac702609ece14a17dac6f8596d80b13300
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-07-09 05:13:46 +0100

    doing some testing on why module 13 not running

--- 9-DYNAMIC-ROUTING/app.js
+++ 9-DYNAMIC-ROUTING/app.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const errorController = require('./controllers/error');
 
 const app = express();
-const port = 5000
+const port = 5500
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
