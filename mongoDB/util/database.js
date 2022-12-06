7ede5bc3a6108caed63746ded30f72c2ce60672b
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-12-06 09:22:14 +0100

    connecting to DB

--- mongoDB/util/database.js
+++ mongoDB/util/database.js
@@ -1,8 +1,20 @@
-const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+const mongoDB = require('mongodb')
 
-module.exports = sequelize;
+const mongoDBClient = mongoDB.MongoClient
+
+const mongoConnect = (callback) => {
+  mongoDBClient
+		.connect(
+			"mongodb+srv://charles-junior:junior2002@cluster0.pazzesj.mongodb.net/test"
+		)
+		.then((client) => {
+			console.log("connected to the DB");
+      callback(client)
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+}
+
+module.exports = mongoConnect
