6b599d6935905829a981586c42352e6ae322af34
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-12-24 23:25:28 +0100

    editing and deleting products

--- mongoDB/models/product.js
+++ mongoDB/models/product.js
@@ -2,18 +2,26 @@ const getDb = require('../util/database').getDb;
 const mongodb = require('mongodb')
 
 class Product {
-  constructor(title, price, description, imageUrl) {
+  constructor(title, price, description, imageUrl,id) {
     this.title = title;
     this.price = price;
     this.description = description;
     this.imageUrl = imageUrl;
+    this._id = id
   }
 
   save() {
-    const db = getDb();
-    return db
+    const db = getDb()
+    let dbOp
+    if(this._id) {
+      // update the product or update it
+      dbOp = db.collection('products').updateOne({_id:new mongodb.ObjectId(this._id)},{$set:this})
+    }else{
+      dbOp = db
       .collection('products')
       .insertOne(this)
+    }
+    return dbOp
       .then(result => {
         console.log(result);
       })
@@ -44,6 +52,19 @@ class Product {
       return product
     } ).
     catch(err => {
+      console.log(err)
+    } )
+  }
+
+  static deleteById(prodId) {
+    const db = getDb()
+    return db
+    .collection('products')
+    .deleteOne({_id:new mongodb.ObjectId(prodId) })
+    .then( result => {
+      console.log('Deleted')
+    } )
+    .catch(err => {
       console.log(err)
     } )
   }
