f6b75ef6de3bac6348f2eef33ac1417fe452dd73
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-12-24 22:02:48 +0100

    fetch one product

--- mongoDB/models/product.js
+++ mongoDB/models/product.js
@@ -1,4 +1,5 @@
 const getDb = require('../util/database').getDb;
+const mongodb = require('mongodb')
 
 class Product {
   constructor(title, price, description, imageUrl) {
@@ -34,6 +35,17 @@ class Product {
       .catch(err => {
         console.log(err);
       });
+  }
+
+  static findById(prodId) {
+    const db = getDb()
+    return db.collection('products').find({_id: new mongodb.ObjectId(prodId) }).next().then(product => {
+      console.log(product)
+      return product
+    } ).
+    catch(err => {
+      console.log(err)
+    } )
   }
 }
 
