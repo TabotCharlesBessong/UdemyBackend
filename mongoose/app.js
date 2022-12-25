40ac987a182d434f6bf0338fe866fabf3b82939a
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-12-25 19:44:52 +0100

    all thing de chacks

--- mongoose/app.js
+++ mongoose/app.js
@@ -6,11 +6,11 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-const User = require('./models/user');
+// const User = require('./models/user');
 
 const app = express();
+mongoose.set("strictQuery", true);
 dotenv.config()
-// mongoose.set("strictQuery", false);
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -21,14 +21,14 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-  User.findById('63a78040fb439061d158f936')
-    .then(user => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
-      next();
-    })
-    .catch(err => console.log(err));
-});
+// app.use((req, res, next) => {
+//   User.findById('5baa2528563f16379fc8a610')
+//     .then(user => {
+//       req.user = new User(user.name, user.email, user.cart, user._id);
+//       next();
+//     })
+//     .catch(err => console.log(err));
+// });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
