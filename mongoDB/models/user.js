c70c88d7f21a72e0cf0318e7ffd571539b05aa1b
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-12-25 11:38:07 +0100

    storing multiple product in a cart

--- mongoDB/models/user.js
+++ mongoDB/models/user.js
@@ -21,12 +21,27 @@ class User {
 	}
 
 	addToCart(product) {
-    // const cartProduct = this.cart.items.findIndex(cp => {
-		// 	return cp._id === product._id
-		// } )
+    const cartProductIndex = this.cart.items.findIndex(cp => {
+			return cp.productId.toString() === product._id.toString()
+		} )
+		let newQuantity = 1
+		
+		const updatedCartItems = [...this.cart.items];
+		
+		if (cartProductIndex >= 0){
+			newQuantity = this.cart.items[cartProductIndex].quantity + 1
+			updatedCartItems[cartProductIndex].quantity = newQuantity
+		}else{
+			// newQuantity = 1
+      updatedCartItems.push({
+        productId: product._id,
+        quantity: newQuantity
+      })
+		}
 		// product.quantity = 1
 		// alternatively
-    const updatedCart = {items:[{productId: new mongodb.ObjectId(product._id) ,quantity:1}]}
+		
+    const updatedCart = {items: updatedCartItems }
 		const db = getDb()
 		return db
 		.collection('users')
