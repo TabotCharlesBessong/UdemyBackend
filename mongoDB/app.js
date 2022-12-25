79df7e8fc07100e5934f88c3e6dea8f044d82be3
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-12-25 11:09:38 +0100

    add to cart functionality

--- mongoDB/app.js
+++ mongoDB/app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   User.findById("63a78040fb439061d158f936")
 		.then((user) => {
-			req.user = user;
+			req.user = new User(user.name,user.email,user.cart,user._id)
 			next();
 		})
 		.catch((err) => console.log(err));
