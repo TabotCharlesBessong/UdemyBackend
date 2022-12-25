9d5a722f2502b4751e9ce10d93ef12c0ecfe0477
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-12-25 16:26:56 +0100

    displaying order

--- mongoDB/models/user.js
+++ mongoDB/models/user.js
@@ -81,9 +81,17 @@ class User {
 
 	addOrder() {
 		const db = getDb()
-		db
-		.collection('orders')
-		.insertOne(this.cart)
+		return this.getCart().then( products => {
+
+			const order = {
+				items:products,
+				user:{
+					_id: new mongodb.ObjectId(this._id),
+					name: this.name,
+				}
+			}
+			return db.collection("orders").insertOne(order)
+		} )
 		.then(result => {
 			this.cart = {items:[]}
 			return db
@@ -92,6 +100,11 @@ class User {
 			{$set:{cart:{items:[]}}}
 			)
 		})
+	}
+
+	getOrders () {
+		const db = getDb()
+		return db.collection('orders').find({'user._id':new mongodb.ObjectId(this._id)}).toArray()
 	}
 	static findById(userId) {
 		const db = getDb();
