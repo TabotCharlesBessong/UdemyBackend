4934adde98e82c4fa51ee4e9a6cc5484a1a3fa70
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-12-26 17:24:13 +0100

    edit product

--- mongoose/controllers/admin.js
+++ mongoose/controllers/admin.js
@@ -59,16 +59,15 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
-
-  const product = new Product(
-    updatedTitle,
-    updatedPrice,
-    updatedDesc,
-    updatedImageUrl,
-    prodId
-  );
-  product
-    .save()
+  Product.findById(prodId)
+   .then(product => {
+     product.title = updatedTitle
+     product.price = updatedPrice
+     product.imageUrl = updatedImageUrl
+     product.description = updatedDesc
+     
+     return product.save()
+   })
     .then(result => {
       console.log('UPDATED PRODUCT!');
       res.redirect('/admin/products');
@@ -77,7 +76,7 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
+  Product.find()
     .then(products => {
       res.render('admin/products', {
         prods: products,
