8b634535f1f9e9e25dc64f99c9eb8514b3eb64e4
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-12-27 11:00:31 +0100

    remove product from cart

--- mongoose/controllers/shop.js
+++ mongoose/controllers/shop.js
@@ -48,7 +48,6 @@ exports.getIndex = (req, res, next) => {
 exports.getCart = (req, res, next) => {
   req.user
     .populate('cart.items.productId')
-    // .execPopulate()
     .then(user => {
       const products = user.cart.items
       res.render('shop/cart', {
@@ -75,7 +74,7 @@ exports.postCart = (req, res, next) => {
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
   req.user
-    .deleteItemFromCart(prodId)
+    .removeFromCart(prodId)
     .then(result => {
       res.redirect('/cart');
     })
