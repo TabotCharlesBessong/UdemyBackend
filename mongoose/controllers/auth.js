776bc8b6d1684a7887e69de9acbb8bb97fd6c6ec
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-12-27 19:58:34 +0100

    logout functionality by deleting session

--- mongoose/controllers/auth.js
+++ mongoose/controllers/auth.js
@@ -1,3 +1,6 @@
+
+const User = require('../models/user')
+
 exports.getLogin = (req, res, next) => {
   // const isLoggedIn = req.get('Cookie').split(';')[3].trim().split('=')[1] === 'true'
 	console.log(req.session.isLoggedIn)
@@ -9,6 +12,20 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-  req.session.isLoggedIn = true
-	res.redirect('/')
+	User.findById("63a9d5e0322e32629361bf94")
+	  .then(user =>{
+			req.session.isLoggedIn = true
+			req.session.user = user
+			res.redirect('/')
+		})
+		.catch(err => {
+			console.log(err)
+		})
+};
+
+exports.postLogout = (req, res, next) => {
+	req.session.destroy((err)=>{
+		console.log(err)
+		res.redirect('/')
+	})
 };
