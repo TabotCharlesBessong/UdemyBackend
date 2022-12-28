93e596a64962cfd3258e83e85ab27e1fe3cfd758
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-12-28 21:26:47 +0100

    login functionality

--- mongoose/controllers/auth.js
+++ mongoose/controllers/auth.js
@@ -18,14 +18,29 @@ exports.getSignup = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-  User.findById("63a9d5e0322e32629361bf94")
+  const email = req.body.email
+  const password = req.body.password
+  User.findOne({email:email})
 		.then((user) => {
-			req.session.isLoggedIn = true;
-			req.session.user = user;
-			req.session.save((err)=>{
-				console.log(err);
-				res.redirect("/");
-			})
+      if(!user) {
+        return res.redirect('/login')
+      }
+      bcrypt.compare(password,user.password)
+      .then(doMatch => {
+        if(doMatch){
+          req.session.isLoggedIn = true;
+					req.session.user = user;
+					return req.session.save((err) => {
+						console.log(err);
+            res.redirect('/')
+					});
+        }
+        res.redirect('/login')
+      })
+      .catch(err => {
+        console.log(err)
+        res.redirect('/login')
+      })
 		})
 		.catch((err) => console.log(err));
 };
